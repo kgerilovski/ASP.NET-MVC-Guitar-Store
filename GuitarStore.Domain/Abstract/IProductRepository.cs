df2f890de1fe15c13b000025970233c8d1e1b777
Kristiyan <k.gerilovski@gmail.com>
2017-10-06 20:28:51 +0300

    Added Admin CRUD operations

--- GuitarStore.Domain/Abstract/IProductRepository.cs
+++ GuitarStore.Domain/Abstract/IProductRepository.cs
@@ -1,14 +1,13 @@
 ﻿using GuitarStore.Domain.Entities;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GuitarStore.Domain.Abstract
 {
     public interface IProductRepository
     {
         IEnumerable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+        Product DeleteProduct(int productId);
     }
 }
