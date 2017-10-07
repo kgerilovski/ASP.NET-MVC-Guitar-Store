10210d22b739ad1f92413d2963bed34dceaa8bc7
Kristiyan <k.gerilovski@gmail.com>
2017-10-07 22:26:16 +0300

    Added user roles

---
+++ GuitarStore.Domain/Concrete/EmailSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace GuitarStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "k.gerilovski@gmail.com";
+        public string MailFromAddress = "k.gerilovski@gmail.com";
+        public bool UseSsl = true;
+        public string Username = "k.gerilovski@gmail.com";
+        public string Password = "owfslzsmptctdkbd";
+        public string ServerName = "smtp.gmail.com";
+        public int ServerPort = 587;
+    }
+}
---
+++ GuitarStore.WebUi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GuitarStore.WebUi.MvcApplication" Language="C#" %>
