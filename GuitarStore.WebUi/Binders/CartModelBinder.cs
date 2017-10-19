df4a0d01223c7298c72d0f6a334df3a716213960
kgerilovski <k.gerilovski@gmail.com>
2017-10-19 21:20:38 +0300

    Removed some unnecessary stuff

--- GuitarStore.WebUi/Binders/CartModelBinder.cs
+++ GuitarStore.WebUi/Binders/CartModelBinder.cs
@@ -1,8 +1,4 @@
 ﻿using GuitarStore.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GuitarStore.WebUi.Binders
